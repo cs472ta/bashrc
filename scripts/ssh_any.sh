9f1d4e0f8981761456fd4a2aeb273dbe08571a51
Taylor <tahlor@gmail.com>
2020-05-08 09:29:10 -0600

    update

--- scripts/ssh_any.sh
+++ scripts/ssh_any.sh
@@ -4,14 +4,18 @@ USER=${2-taylor}
 DEST_NETWORK=${3-SCHOOL} # destination
 CURRENT_NETWORK=${4-UNSPECIFIED} # - LOCAL/REMOTE
 ON_HOME=false;
+ON_SCHOOL=false;
 
 if [ $CURRENT_NETWORK = "UNSPECIFIED" ]; then
 	if [[ $(route -n | grep 'UG[ \t]' | awk '{print $2}') =~ 192.168.18[78].1 ]]; then # on home network
 		ON_HOME=true;
+	elif [[ $(route -n | grep 'UG[ \t]' | awk '{print $2}') =~ 192.168.29.1 ]]; then
+		ON_SCHOOL=true;
 	fi;
+	
 	if $ON_HOME && [ $DEST_NETWORK = 'HOME' ]; then
 		CURRENT_NETWORK="LOCAL"
-	elif ! $ON_HOME && [ $DEST_NETWORK = 'SCHOOL' ]; then
+	elif $ON_SCHOOL && [ $DEST_NETWORK = 'SCHOOL' ]; then
 		CURRENT_NETWORK="LOCAL"
 	else
 		CURRENT_NETWORK="REMOTE"
@@ -28,13 +32,13 @@ elif [ $hostname = 'pi3' ]; then
 	if [ "$(iwgetid -r)" = "FifeNet" ]; then ssh pi@192.168.187.103; else ssh pi@fife.entrydns.org -p 57321; fi
 elif [ $hostname = 'pi2' ]; then
 	if [ "$(iwgetid -r)" = "FifeNet" ]; then ssh pi@192.168.187.98; else ssh pi@fife.entrydns.org -p 57323; fi
-elif [ "$CURRENT_NEWORK" = 'LOCAL' ] ; then
+elif [ "$CURRENT_NETWORK" = 'LOCAL' ] ; then
 	echo "on local network"
 	ssh $USER@$hostname
-elif [ "$CURRENT_NEWORK" = "REMOTE" ] && [ $DEST_NETWORK = "SCHOOL" ]; then
+elif [ "$CURRENT_NETWORK" = "REMOTE" ] && [ $DEST_NETWORK = "SCHOOL" ]; then
 	echo "on home network, connecting to school"
 	ssh -t tarch@schizo.cs.byu.edu "ssh $USER@$hostname";
-elif [ "$CURRENT_NEWORK" = "REMOTE" ] && [ $DEST_NETWORK = "HOME" ]; then
+elif [ "$CURRENT_NETWORK" = "REMOTE" ] && [ $DEST_NETWORK = "HOME" ]; then
 	echo "on school network, connecting to home"
 	ssh -t pi@fife.entrydns.org -p 57321 "ssh $USER@$hostname";
 fi
