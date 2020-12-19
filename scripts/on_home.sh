fcf504121e1f2ef21d5d49e5b23acc9f4b56c8d6
Taylor <tahlor@gmail.com>
2020-12-19 01:02:17 -0700

    update

--- scripts/on_home.sh
+++ scripts/on_home.sh
@@ -1,8 +1,14 @@
-#!/bin/bash 
- 
+#!/bin/bash
+
+if ! command -v ifconfig &> /dev/null
+then
+    alias ifconfig=ipconfig
+fi
+
 on_home() { 
+
 ip=${1-192.168.187} 
-if [[ $(ipconfig | grep $1) ]]; then 
+if [[ $(ifconfig | grep $1) ]]; then 
  return 0; 
 else 
  return 1; 
