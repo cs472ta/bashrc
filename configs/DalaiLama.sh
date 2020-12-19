e033b3a34034083dd8ba6fbfda0db8f8c4d399bc
Taylor <tahlor@gmail.com>
2020-12-19 11:29:21 -0700

    update

--- configs/DalaiLama.sh
+++ configs/DalaiLama.sh
@@ -2,3 +2,6 @@ ONEDRIVE="/media/taylor/Data/OneDrive"
 GITHUB="/media/taylor/Data/Linux/Github"
 HWR_ENV="hwr"
 BASHRC="/home/taylor/bashrc"
+
+
+alias boot_windows="sudo grub-reboot 2 && sudo reboot"
--- configs/pythagoras.sh
+++ configs/pythagoras.sh
@@ -1,3 +1,5 @@
 GITHUB=~/github
 ONEDRIVE=/data/OneDrive
 HWR_ENV=hwr
+
+alias boot_windows="sudo grub-reboot 2 && sudo reboot"
