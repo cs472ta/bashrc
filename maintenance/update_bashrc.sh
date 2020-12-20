72b98366e5e7b75503a15008a0fd696eca2312f1
Taylor <tahlor@gmail.com>
2020-12-20 12:17:34 -0700

    update

--- maintenance/update_bashrc.sh
+++ maintenance/update_bashrc.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 
+cron_command="0 6 * * * cd ~/bashrc && ./maintenance/update_bashrc.sh > ./cron.log 2>&1"
+
+../scripts/add_to_cron.sh "$cron_command"
+
+# Add bashrc to sources
+if [ -f ~/.bashrc ]; then
+    KEEP_EN="source /home/$LOCAL_USER/bashrc/master.sh"
+    grep -q "$KEEP_EN" /home/$LOCAL_USER/bashrc/master.sh
+    if [ $? -ne 0 ]; then
+        echo "source /home/$LOCAL_USER/bashrc/master.sh" >> ~/.bashrc
+    fi
+fi
+
+
 message=${1:-"update"}
 git add . && git commit -m "$message" && git push
 git pull
