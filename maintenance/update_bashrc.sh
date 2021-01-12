1b3525aa7ab3b629e3bcbea2f14b321892f6c332
Taylor Archibald <tahlor@gmail.com>
2021-01-12 12:02:48 -0700

    update

--- maintenance/update_bashrc.sh
+++ maintenance/update_bashrc.sh
@@ -2,15 +2,15 @@
 
 ## Add this to CRON -- backup the repo at 6am
 cron_command="0 6 * * * cd ~/bashrc && ./maintenance/update_bashrc.sh > ./cron.log 2>&1"
-logname=/usr/bin/logname
+logname=$(/usr/bin/logname)
 
 ## Backup the old CRON in the repo
 ~/bashrc/scripts/add_to_cron.sh "$cron_command"
 
 ## Add bashrc to sources
 if [[ -f ~/.bashrc ]]; then
-    KEEP_EN="source /home/$($logname)/bashrc/master.sh"
-    grep -q "$KEEP_EN" /home/$($logname)/.bashrc
+    KEEP_EN="source /home/$(logname)/bashrc/master.sh"
+    grep -q "$KEEP_EN" /home/$(logname)/.bashrc
     if [[ $? -ne 0 ]]; then
         echo "$KEEP_EN" >> ~/.bashrc
     fi
