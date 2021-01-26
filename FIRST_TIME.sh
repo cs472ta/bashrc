b3221662b1a4afc20ed3e2b1f847bcf46a1441c0
Taylor Archibald <tahlor@gmail.com>
2021-01-26 09:43:58 -0700

    update

--- FIRST_TIME.sh
+++ FIRST_TIME.sh
@@ -4,8 +4,11 @@ sudo apt install openssh-server
 sudo apt install git
 sudo ufw allow ssh
 
+LOCAL_USER="$(logname)"
+LOCAL_HOST="$(hostname)"
+
 sudo apt install xclip
-ssh-keygen -t rsa -N '' -b 4096 -C "tahlor@gmail.com" -f ~/.ssh/id_rsa
+ssh-keygen -t rsa -N '' -b 4096 -C  "${LOCAL_USER}@${LOCAL_HOST}" -f ~/.ssh/id_rsa
 cat ~/.ssh/id_rsa.pub | xclip -sel clip
 
 # ADD SSH KEY TO GITHUB 
