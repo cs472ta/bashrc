c2a72b070fae76bfcb2cc3af9e69e45981006515
Taylor Archibald <tahlor@gmail.com>
2021-01-26 14:42:05 -0700

    update

--- configs/axon.sh
+++ configs/axon.sh
@@ -1,3 +1,8 @@
 alias www='cd /var/www'
 alias wordpress='cd /var/www/wp-content'
 alias wordpress_dev='cd /var/www/dev/wordpress'
+
+
+alias lock_ftp="sudo sed -i 's~'\''direct'\''~'\''ftpext'\''~g' /var/www/wp-config.php"
+alias unlock_ftp="sudo sed -i 's~'\''ftpext'\''~'\''direct'\''~g' /var/www/wp-config.php"
+
